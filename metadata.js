e0da09219140cd9e7830b9db8766a26be246fb8a
Alexander Plavin <alexander@plav.in>
2026-02-13 23:40:36 +0000

    Deploy readme example to GitHub Pages

---
+++ metadata.js
@@ -0,0 +1,102 @@
+const METADATA = {
+    "controls": [
+        {
+            "values": [
+                1,
+                2,
+                3,
+                4
+            ],
+            "name": "frequency"
+        },
+        {
+            "values": [
+                0,
+                0.2,
+                0.5,
+                1
+            ],
+            "name": "p"
+        }
+    ],
+    "blocks": [
+        {
+            "size": {
+                "height": 575,
+                "width": 846
+            }
+        },
+        {
+            "size": {
+                "height": 586,
+                "width": 846
+            }
+        }
+    ],
+    "snapshots": [
+        {
+            "frequency": 1,
+            "p": 0
+        },
+        {
+            "frequency": 2,
+            "p": 0
+        },
+        {
+            "frequency": 3,
+            "p": 0
+        },
+        {
+            "frequency": 4,
+            "p": 0
+        },
+        {
+            "frequency": 1,
+            "p": 0.2
+        },
+        {
+            "frequency": 2,
+            "p": 0.2
+        },
+        {
+            "frequency": 3,
+            "p": 0.2
+        },
+        {
+            "frequency": 4,
+            "p": 0.2
+        },
+        {
+            "frequency": 1,
+            "p": 0.5
+        },
+        {
+            "frequency": 2,
+            "p": 0.5
+        },
+        {
+            "frequency": 3,
+            "p": 0.5
+        },
+        {
+            "frequency": 4,
+            "p": 0.5
+        },
+        {
+            "frequency": 1,
+            "p": 1
+        },
+        {
+            "frequency": 2,
+            "p": 1
+        },
+        {
+            "frequency": 3,
+            "p": 1
+        },
+        {
+            "frequency": 4,
+            "p": 1
+        }
+    ]
+};
